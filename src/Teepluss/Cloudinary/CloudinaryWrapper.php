27e1ce1bc18b7223989b6725af016a2418104dde
Matthew Fawcett <mail@matthewfawcett.co.uk>
2016-02-25 10:34:05 +0000

    Use dot syntax to access config variables

--- src/Teepluss/Cloudinary/CloudinaryWrapper.php
+++ src/Teepluss/Cloudinary/CloudinaryWrapper.php
@@ -48,9 +48,9 @@ class CloudinaryWrapper {
         $this->config = $config;
 
         $this->cloudinary->config(array(
-            'cloud_name' => $this->config->get('cloudinary::cloudName'),
-            'api_key'    => $this->config->get('cloudinary::apiKey'),
-            'api_secret' => $this->config->get('cloudinary::apiSecret')
+            'cloud_name' => $this->config->get('cloudinary.cloudName'),
+            'api_key'    => $this->config->get('cloudinary.apiKey'),
+            'api_secret' => $this->config->get('cloudinary.apiSecret')
         ));
     }
 
@@ -128,7 +128,7 @@ class CloudinaryWrapper {
      */
     public function show($publicId, $options = array())
     {
-        $defaults = $this->config->get('cloudinary::scaling');
+        $defaults = $this->config->get('cloudinary.scaling');
 
         $options = array_merge($defaults, $options);
 
